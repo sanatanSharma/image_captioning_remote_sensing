c439819d8d13962ed5a2f53e785aa1e5567355f5
Sanatan Sharma <sanatansharma1996@gmail.com>
2018-02-15 17:16:33 +0530

    New Training files

--- resize.py
+++ resize.py
@@ -25,20 +25,16 @@ def main():
         resized_folder = './image/%s2014_resized/' %split
         if not os.path.exists(resized_folder):
             os.makedirs(resized_folder)
-        print('Start resizing %s images.' %split)
+        print ('Start resizing %s images.' %split)
         image_files = os.listdir(folder)
         num_images = len(image_files)
         for i, image_file in enumerate(image_files):
             with open(os.path.join(folder, image_file), 'r+b') as f:
-                try:
-                    with Image.open(f) as image:
-                        image = resize_image(image)
-                        image.save(os.path.join(resized_folder, image_file), image.format)
-                except:
-                    print("Some error in processing image ", f)
-                    continue
+                with Image.open(f) as image:
+                    image = resize_image(image)
+                    image.save(os.path.join(resized_folder, image_file), image.format)
             if i % 100 == 0:
-                print('Resized images: %d/%d' %(i, num_images))
+                print ('Resized images: %d/%d' %(i, num_images))
               
             
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -9,17 +9,23 @@ def main():
     word_to_idx = data['word_to_idx']
     # load val dataset to print out bleu scores every epoch
     val_data = load_coco_data(data_path='./data', split='val')
+    data_test = load_coco_data(data_path='./data', split='test')
 
     model = CaptionGenerator(word_to_idx, dim_feature=[196, 512], dim_embed=512,
                                        dim_hidden=1024, n_time_step=16, prev2out=True, 
                                                  ctx2out=True, alpha_c=1.0, selector=True, dropout=True)
 
-    solver = CaptioningSolver(model, data, val_data, n_epochs=20, batch_size=128, update_rule='adam',
+    solver = CaptioningSolver(model, data, val_data, n_epochs=2, batch_size=128, update_rule='adam',
+                                          learning_rate=0.001, print_every=100, save_every=1, image_path='./image/',
+                                    pretrained_model='model/lstm/model-20', model_path='model/lstm/', test_model='model/lstm/model-10',
+                                     print_bleu=True, log_path='log/')
+    '''solver = CaptioningSolver(model, data, val_data, n_epochs=20, batch_size=128, update_rule='adam',
                                           learning_rate=0.001, print_every=1000, save_every=1, image_path='./image/',
                                     pretrained_model=None, model_path='model/lstm/', test_model='model/lstm/model-10',
-                                     print_bleu=True, log_path='log/')
+                                     print_bleu=True, log_path='log/')'''
 
-    solver.train()
+    #solver.train()
+    solver.test(data)
 
 if __name__ == "__main__":
     main()
